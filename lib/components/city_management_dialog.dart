1a57957b9604fdd266ccafe0111957fb6dd8d8ec
Doomsdhg <tohanknv@yandex.ru>
2022-08-23 15:08:47 +0300

    WTHR-06: autocomplete options styles implementation

--- lib/components/city_management_dialog.dart
+++ lib/components/city_management_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/constants/constants.dart';
 import 'package:weather_app/services/api/weather_api.dart';
 
 class CityManagementDialog {
@@ -35,6 +36,61 @@ class _AutoCompleteInput extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Autocomplete<String>(
+      fieldViewBuilder: ((context, textEditingController, focusNode, onFieldSubmitted){
+        return TextField(
+          controller: textEditingController,
+          focusNode: focusNode,
+          onEditingComplete: onFieldSubmitted,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5)
+            )
+          ),
+        );
+      }),
+      optionsViewBuilder: (context, onSelected, options) {
+          return FractionallySizedBox(
+            widthFactor: 0.65,
+            alignment: Alignment.topLeft,
+            child: Column(
+              children: [
+                Material(
+                  child: ListView.separated(
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    separatorBuilder: (context, index) => Divider(height: 2, color: Colors.grey,),
+                    itemCount: options.length,
+                    itemBuilder: (context, index){
+                      return Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade100
+                        ),
+                        alignment: Alignment.topLeft,
+                        child: Container(
+                          padding: EdgeInsets.all(5),
+                          child: GestureDetector(
+                            onTap: () {
+                              onSelected(options.elementAt(index));
+                            },
+                            child: ListTile(
+                              title: Text(
+                                options.elementAt(index),
+                                style: TextStyle(
+                                  fontSize: FontConstants.SMALL_SIZE
+                                ),  
+                              ),
+                            )
+                          ),
+                        )
+                      );
+                    }
+                  )
+
+                ),
+              ],
+            ),
+          );
+      },
       displayStringForOption: _displayStringForOption,
       onSelected: (String value){
         inputCallback(value);
@@ -49,6 +105,8 @@ class _AutoCompleteInput extends StatelessWidget {
   }
 
   static String _displayStringForOption(String option) => option;
+
+
 
   Future<Iterable<String>> _findCities(String input) async {
     Iterable<String> citiesList = await WeatherApi().findCities(input);
